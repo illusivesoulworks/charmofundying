e7fb977cb123f64743807c10fdfc737d2400f3e6
TheIllusiveC4 <29991504+TheIllusiveC4@users.noreply.github.com>
2021-05-13 20:29:43 -0700

    switch to mixins, closes #3

--- src/main/java/top/theillusivec4/curioofundying/CurioOfUndying.java
+++ src/main/java/top/theillusivec4/curioofundying/CurioOfUndying.java
@@ -34,15 +34,12 @@ import net.minecraft.item.Items;
 import net.minecraft.potion.EffectInstance;
 import net.minecraft.potion.Effects;
 import net.minecraft.stats.Stats;
-import net.minecraft.util.DamageSource;
 import net.minecraft.util.Direction;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.capabilities.ICapabilityProvider;
 import net.minecraftforge.common.util.LazyOptional;
 import net.minecraftforge.event.AttachCapabilitiesEvent;
-import net.minecraftforge.event.entity.living.LivingDeathEvent;
-import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.InterModComms;
@@ -50,8 +47,10 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.commons.lang3.tuple.ImmutableTriple;
 import top.theillusivec4.curios.api.CuriosApi;
 import top.theillusivec4.curios.api.CuriosCapability;
+import top.theillusivec4.curios.api.SlotContext;
 import top.theillusivec4.curios.api.SlotTypeMessage;
 import top.theillusivec4.curios.api.SlotTypePreset;
 import top.theillusivec4.curios.api.type.capability.ICurio;
@@ -76,6 +75,7 @@ public class CurioOfUndying {
         () -> SlotTypePreset.CHARM.getMessageBuilder().build());
   }
 
+  @SuppressWarnings("unused")
   @SubscribeEvent
   public void attachCapabilities(AttachCapabilitiesEvent<ItemStack> evt) {
 
@@ -85,7 +85,7 @@ public class CurioOfUndying {
     ICurio curio = new ICurio() {
 
       @Override
-      public boolean canRightClickEquip() {
+      public boolean canEquipFromUse(SlotContext ctx) {
         return true;
       }
 
@@ -96,9 +96,11 @@ public class CurioOfUndying {
 
       @Override
       public void render(String identifier, int index, MatrixStack matrixStack,
-          IRenderTypeBuffer renderTypeBuffer, int light, LivingEntity livingEntity, float limbSwing,
-          float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw,
-          float headPitch) {
+                         IRenderTypeBuffer renderTypeBuffer, int light, LivingEntity livingEntity,
+                         float limbSwing,
+                         float limbSwingAmount, float partialTicks, float ageInTicks,
+                         float netHeadYaw,
+                         float headPitch) {
         ICurio.RenderHelper.translateIfSneaking(matrixStack, livingEntity);
         ICurio.RenderHelper.rotateIfSneaking(matrixStack, livingEntity);
         matrixStack.scale(0.35F, 0.35F, 0.35F);
@@ -115,53 +117,34 @@ public class CurioOfUndying {
       @Nonnull
       @Override
       public <T> LazyOptional<T> getCapability(@Nonnull Capability<T> cap,
-          @Nullable Direction side) {
+                                               @Nullable Direction side) {
         return CuriosCapability.ITEM.orEmpty(cap, curioOpt);
       }
     };
     evt.addCapability(CuriosCapability.ID_ITEM, provider);
   }
 
-  @SubscribeEvent(priority = EventPriority.HIGHEST)
-  public void onLivingDeath(LivingDeathEvent evt) {
+  public static boolean hasTotem(LivingEntity livingEntity) {
+    ItemStack stack =
+        CuriosApi.getCuriosHelper().findEquippedCurio(Items.TOTEM_OF_UNDYING, livingEntity).map(
+            ImmutableTriple::getRight).orElse(ItemStack.EMPTY);
 
-    if (hasTotemProtection(evt.getEntityLiving(), evt.getSource())) {
-      evt.setCanceled(true);
-    }
-  }
+    if (!stack.isEmpty()) {
+      stack.shrink(1);
 
-  private boolean hasTotemProtection(LivingEntity livingEntity, DamageSource source) {
-
-    if (source.canHarmInCreative()) {
-      return false;
-    }
-
-    for (ItemStack held : livingEntity.getHeldEquipment()) {
-
-      if (held.getItem() == Items.TOTEM_OF_UNDYING) {
-        return false;
+      if (livingEntity instanceof ServerPlayerEntity) {
+        ServerPlayerEntity serverplayerentity = (ServerPlayerEntity) livingEntity;
+        serverplayerentity.addStat(Stats.ITEM_USED.get(Items.TOTEM_OF_UNDYING));
+        CriteriaTriggers.USED_TOTEM.trigger(serverplayerentity, stack);
       }
+      livingEntity.setHealth(1.0F);
+      livingEntity.clearActivePotions();
+      livingEntity.addPotionEffect(new EffectInstance(Effects.REGENERATION, 900, 1));
+      livingEntity.addPotionEffect(new EffectInstance(Effects.ABSORPTION, 100, 1));
+      livingEntity.addPotionEffect(new EffectInstance(Effects.FIRE_RESISTANCE, 800, 0));
+      livingEntity.world.setEntityState(livingEntity, (byte) 35);
+      return true;
     }
-    return CuriosApi.getCuriosHelper().findEquippedCurio(Items.TOTEM_OF_UNDYING, livingEntity)
-        .map(totem -> {
-          this.activateTotem(livingEntity, totem.getRight());
-          return true;
-        }).orElse(false);
-  }
-
-  private void activateTotem(LivingEntity livingEntity, ItemStack totem) {
-    ItemStack copy = totem.copy();
-    totem.shrink(1);
-
-    if (livingEntity instanceof ServerPlayerEntity) {
-      ServerPlayerEntity serverPlayer = (ServerPlayerEntity) livingEntity;
-      serverPlayer.addStat(Stats.ITEM_USED.get(Items.TOTEM_OF_UNDYING));
-      CriteriaTriggers.USED_TOTEM.trigger(serverPlayer, copy);
-    }
-    livingEntity.setHealth(1.0F);
-    livingEntity.clearActivePotions();
-    livingEntity.addPotionEffect(new EffectInstance(Effects.REGENERATION, 900, 1));
-    livingEntity.addPotionEffect(new EffectInstance(Effects.ABSORPTION, 100, 1));
-    livingEntity.world.setEntityState(livingEntity, (byte) 35);
+    return false;
   }
 }
